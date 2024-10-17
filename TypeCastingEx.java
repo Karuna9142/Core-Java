dfff763eb8de9531a953034af74fe3a49f5b0b86
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-17 14:28:56 +0530

    Add files via upload

---
+++ TypeCastingEx.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class TypeCastingEx {
+
+	public static void main(String[] args) {
+		
+		int num = 4;
+		
+		double num1 =  (double)num; //typecasting
+	
+		System.out.println("Integer value:- "+num);
+		System.out.println("Double value:- "+num1);
+	}
+
+}
