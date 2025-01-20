5550b1b545fab73ad04ca55b137a92eacaf6fc8a
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-01-20 14:13:43 +0530

    Add files via upload

---
+++ WhileEx.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class WhileEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int i = 1;
+    while(i<=100)
+    {
+    	if(i%2==0)
+    		System.out.print(i+ ",");
+    	i++;
+    }
+	}
+
+}
