1a528bd8faeaababcde3aa930149e44658b0cedd
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-01-20 14:12:27 +0530

    Add files via upload

---
+++ basic/BigTen.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class BigTen {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n,i = 1,big = 0;
+		while(i<=5) {
+			System.out.println("Enter a number- ");
+			n = sc.nextInt();
+			
+			if(n>big) 
+				big = n;
+		    i++;
+		}
+       System.out.println("Big ="+big);
+	}
+
+}
