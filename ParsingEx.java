837d469df1344555fce5bde738fdddefef168b4b
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-01-25 12:37:20 +0530

    Add files via upload

---
+++ ParsingEx.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class ParsingEx {
+
+	public static void main(String[] args) {
+		
+		String s1 = "243";
+		int n = Integer.parseInt(s1);
+		System.out.println(n); //243
+		
+
+	}
+
+}
